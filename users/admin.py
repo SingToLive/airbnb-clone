386667a7af99b87bd64aff83efe632f514b65838
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-01-11 13:29:33 +0900

    ~#3
    lecture 3 update

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """Custon User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Banana",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
